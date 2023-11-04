77809ffea0b472c638310769d537419344dcfabe
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 14:07:53 +0100

    List items of json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-function App() {
+import { Products } from "./components/Products";
+import { products } from "./mock/products.json";
 
+function App() {
   return (
     <>
       <h1>Shopping card</h1>
+      <Products products={products} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
