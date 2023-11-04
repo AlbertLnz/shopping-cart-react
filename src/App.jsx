12719865cf832ce728e0afe510d0739eb95d63b6
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 16:01:42 +0100

    useContext implemented & only one global state for minPrice

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Footer } from "./components/Footer.jsx";
 import { Header } from "./components/Header";
 import { Products } from "./components/Products";
@@ -7,13 +6,12 @@ import { useFilters } from "./hooks/useFilters.js";
 import { products as initialProducts } from "./mock/products.json";
 
 function App() {
-  const [products] = useState(initialProducts);
-  const { filters, filterProducts, setFilters } = useFilters();
-  const filteredProducts = filterProducts(products);
+  const { filters, filterProducts } = useFilters();
+  const filteredProducts = filterProducts(initialProducts);
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer filters={filters} />}
     </>
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,21 +1,22 @@
-import React, { useId, useState } from "react";
+import React, { useId } from "react";
+import { useFilters } from "../hooks/useFilters";
 import "./Filters.css";
 
-export function Filters({ onChange }) {
-  const [minPrice, setMinPrice] = useState(0);
+export function Filters() {
+  const { filters, setFilters } = useFilters();
+
   const minPriceFilterId = useId();
   const categoryFilterId = useId();
 
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value);
-    onChange((prevState) => ({
+    setFilters((prevState) => ({
       ...prevState,
       minPrice: event.target.value,
     }));
   };
 
   const handleChangeCategory = (event) => {
-    onChange((prevState) => ({
+    setFilters((prevState) => ({
       ...prevState,
       category: event.target.value,
     }));
@@ -31,8 +32,9 @@ export function Filters({ onChange }) {
           min="0"
           max="1000"
           onChange={handleChangeMinPrice}
+          value={filters.minPrice}
         />
-        <span>{minPrice}</span>
+        <span>{filters.minPrice}</span>
       </div>
 
       <div>
