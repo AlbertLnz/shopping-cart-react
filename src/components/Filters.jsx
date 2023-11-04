ed51d744bf5c33aa012cdf178be30b1166c8f941
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 14:48:28 +0100

    Filters working but using prop drilling

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import "./Filters.css";
 
-export function Filters() {
+export function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0);
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value);
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    onChange((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
   };
 
   return (
@@ -23,11 +34,11 @@ export function Filters() {
       </div>
 
       <div>
-        <label htmlFor="cateogory">Categoría</label>
-        <select id="category">
+        <label htmlFor="category">Categoría</label>
+        <select id="category" onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="laptops">Portátiles</option>
-          <option value="moviles">Moviles</option>
+          <option value="smartphones">Moviles</option>
         </select>
       </div>
     </section>
