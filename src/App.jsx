d8fbf74f76e0d1a5d83147a2648bd165b4e15153
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 15:34:16 +0100

    Creating a useContext for filters w/ useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Footer } from "./components/Footer.jsx";
 import { Header } from "./components/Header";
 import { Products } from "./components/Products";
+import { FiltersContext } from "./components/context/filters.jsx";
 import { IS_DEVELOPMENT } from "./config.js";
 import { products as initialProducts } from "./mock/products.json";
 
 function useFilters() {
-  const [filters, setFilters] = useState({
-    category: "all",
-    minPrice: 0,
-  });
+  const { filters, setFilters } = useContext(FiltersContext);
 
   const filterProducts = (products) => {
     return products.filter((product) => {
