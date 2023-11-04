1ab371c8300e0260840364a724fff020727e4122
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 15:51:22 +0100

    Export custom hook useFilters

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Footer } from "./components/Footer.jsx";
 import { Header } from "./components/Header";
 import { Products } from "./components/Products";
-import { FiltersContext } from "./components/context/filters.jsx";
 import { IS_DEVELOPMENT } from "./config.js";
+import { useFilters } from "./hooks/useFilters.js";
 import { products as initialProducts } from "./mock/products.json";
-
-function useFilters() {
-  const { filters, setFilters } = useContext(FiltersContext);
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || product.category === filters.category)
-      );
-    });
-  };
-
-  return { filters, filterProducts, setFilters };
-}
 
 function App() {
   const [products] = useState(initialProducts);
