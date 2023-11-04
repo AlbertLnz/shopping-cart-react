a0fc5a4c31beb2ef2952a710086de23129b02c37
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 14:19:46 +0100

    filters working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import { Products } from "./components/Products";
-import { products } from "./mock/products.json";
+import { products as initialProducts } from "./mock/products.json";
 
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
       <h1>Shopping card</h1>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   );
 }
