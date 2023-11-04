0a8b17dafcde9cff3eed829bc84f7d54085bacde
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-04 14:55:52 +0100

    useId hook

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useId, useState } from "react";
 import "./Filters.css";
 
 export function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0);
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value);
@@ -22,10 +24,10 @@ export function Filters({ onChange }) {
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Precio a partir de:</label>
+        <label htmlFor={minPriceFilterId}>Precio a partir de:</label>
         <input
           type="range"
-          id="price"
+          id={minPriceFilterId}
           min="0"
           max="1000"
           onChange={handleChangeMinPrice}
@@ -34,8 +36,8 @@ export function Filters({ onChange }) {
       </div>
 
       <div>
-        <label htmlFor="category">Categoría</label>
-        <select id="category" onChange={handleChangeCategory}>
+        <label htmlFor={categoryFilterId}>Categoría</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="laptops">Portátiles</option>
           <option value="smartphones">Moviles</option>
